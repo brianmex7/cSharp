1ed390eeb5a79e2e1faedc4c23d312dc1fa9eb94
brianmex7 <brianmexico77@gmail.com>
2024-08-07 15:25:42 -0600

    operators

--- DotNet7/Start/Overview/Operators/Program.cs
+++ DotNet7/Start/Overview/Operators/Program.cs
@@ -7,34 +7,44 @@ namespace Operators
         static void Main(string[] args)
         {
             // Declare some variables to excercise the operators
-            int x=10, y=5;
-            string a="abcd", b="efgh";
+            int x = 10, y = 5;
+            string a = "abcd", b = "efgh";
 
             // TODO: Basic math operators are +, -, /, *
             Console.WriteLine("----- Basic Math -----");
+            Console.WriteLine((x / y) * x);
+            Console.WriteLine(a + b);
 
 
             // TODO: Increment / decrement operators
             Console.WriteLine("----- Shorthand -----");
-
+            x++;
+            y--;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             // TODO: Operators can be shorthand: a = a + b
+            Console.WriteLine(a += b);
 
 
             // TODO: Logical operators &&, ||
             Console.WriteLine("----- Logic Operators -----");
+            Console.WriteLine(x > y && y >= 5);
+            Console.WriteLine(x > y || y >= 5);
 
 
             // null-coalescing operators
             string str = null;
             // TODO: the ?? operator uses left operand if not null, or right one if it is
+            Console.WriteLine(str ?? "Unknown Value");
 
-            
+
             // TODO: the ??= operator assigns the right operand if the left one is null
             // it replaces the code:
             // if (variable is null) {
             //    variable = somevalue;
             // }
+            Console.WriteLine(str ??= "Hello");
 
         }
     }
