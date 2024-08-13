06ff9b7ed67738f4d6a73addc16bfff4408d32ad
brianmex7 <brianmexico77@gmail.com>
2024-08-13 17:48:49 -0600

    forLoops

--- DotNet7/Start/Program Flow/Conditional-switch/Program.cs
+++ DotNet7/Start/Program Flow/Conditional-switch/Program.cs
@@ -6,9 +6,31 @@ namespace ConditionalOps
     {
         static void Main(string[] args)
         {
-            int theVal = 50;
+            int theVal = 20;
 
             // TODO: The switch statement
+            switch (theVal)
+            {
+                case 50:
+                    Console.WriteLine("The number is 50");
+                    break;
+                case 40:
+                    Console.WriteLine("The number is not 50");
+                    break;
+                case 30:
+                    Console.WriteLine("The number isn't 50");
+                    break;
+                case 20:
+                    Console.WriteLine("The number ain't 50");
+                    break;
+                case 10:
+                case 0:
+                    Console.WriteLine("The number isn't 50");
+                    break;
+                default:
+                    Console.WriteLine("The number is something else");
+                    break;
+            }
 
         }
     }
