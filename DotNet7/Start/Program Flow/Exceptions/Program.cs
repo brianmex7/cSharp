3a3f54ecb16314666acc1dc2209e9d2d4f488bde
brianmex7 <brianmexico77@gmail.com>
2024-08-14 17:04:49 -0600

    exceptions

--- DotNet7/Start/Program Flow/Exceptions/Program.cs
+++ DotNet7/Start/Program Flow/Exceptions/Program.cs
@@ -6,13 +6,29 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            int x = 100;
+            int x = 600;
             int y = 10;
             int result;
 
             // TODO: try-catch expressions make error checking easier
-            result = x / y;
-            Console.WriteLine("The result is: {0}", result);
+            try {
+                if(x > 1000){
+                    throw new ArgumentOutOfRangeException("x", "x must be less than 1000");
+                }
+                result = x / y;
+                Console.WriteLine("The result is: {0}", result);
+            }
+            catch(DivideByZeroException e){
+                Console.WriteLine("Cannot divide by zero");
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e){
+                Console.WriteLine("1000 is the limit");
+                Console.WriteLine(e.Message);
+            }
+            finally{
+
+            }
         }
     }
 }
