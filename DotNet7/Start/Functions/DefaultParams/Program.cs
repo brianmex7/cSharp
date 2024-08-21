6afd822789335db1555bdeef3124761cc51632ed
brianmex7 <brianmexico77@gmail.com>
2024-08-21 16:50:23 -0600

    param function

--- DotNet7/Start/Functions/DefaultParams/Program.cs
+++ DotNet7/Start/Functions/DefaultParams/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 
 // TODO: Functions can provide default values for their parameters
-void PrintWithPrefix(string thestr, string prefix)
+void PrintWithPrefix(string thestr, string prefix = "")
 {
     Console.WriteLine($"{prefix} {thestr}");
 }
 
 // TODO: Test the default parameters
 
+PrintWithPrefix("Hello World");
+PrintWithPrefix("Hello World", ">???");
+
+
 
 // TODO: Call with named params
-
+PrintWithPrefix(prefix: "%%", thestr: "Hellow World");
