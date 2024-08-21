b892b5ce791944474080af9953fba352f583dc49
brianmex7 <brianmexico77@gmail.com>
2024-08-21 17:57:28 -0600

    C# classes

--- DotNet7/Start/Classes/Defining/Program.cs
+++ DotNet7/Start/Classes/Defining/Program.cs
@@ -7,10 +7,12 @@ namespace Defining
         static void Main(string[] args)
         {
             // TODO: Create new object instances using the "new" operator
+            Book b1 = new Book("One Piece", "Echiro Oda", 1000);
 
-            
+
+
             // TODO: Call a method on the object
-
+            Console.WriteLine(b1.GetDescription());
 
             // TODO: try to set one of the properties -- 
             // this will result in an error
