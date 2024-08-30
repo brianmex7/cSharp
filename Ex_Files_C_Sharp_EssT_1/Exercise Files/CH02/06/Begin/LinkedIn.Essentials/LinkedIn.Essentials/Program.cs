3f5c49423e76563d70ea021ef4452f0ec97633b5
brianmex7 <brianmexico77@gmail.com>
2024-08-29 18:53:49 -0600

    comparisson

--- Ex_Files_C_Sharp_EssT_1/Exercise Files/CH02/06/Begin/LinkedIn.Essentials/LinkedIn.Essentials/Program.cs
+++ Ex_Files_C_Sharp_EssT_1/Exercise Files/CH02/06/Begin/LinkedIn.Essentials/LinkedIn.Essentials/Program.cs
@@ -8,14 +8,14 @@ Records();
 static void Classes()
 {
     Console.WriteLine("=================Classes=================");
-    CPoint p1 = new CPoint {X= 7, Y= 3 };
+    CPoint p1 = new CPoint { X = 7, Y = 3 };
     CPoint p2 = p1;
 
-    Console.WriteLine($"P1 = P2: {p1 == p2}");
+    Console.WriteLine($"P1 = P2: {p1.Equals(p2)}");
 
     CPoint p3 = new CPoint { X = 7, Y = 3 };
 
-    Console.WriteLine($"P1 = P3: {p1 == p3}");
+    Console.WriteLine($"P1 = P3: {p1.Equals(p3)}");
     Console.WriteLine();
 }
 
@@ -25,11 +25,11 @@ static void Structs()
     SPoint p1 = new SPoint { X = 7, Y = 3 };
     SPoint p2 = p1;
 
-    //Console.WriteLine($"P1 = P2: {p1 == p2}");
+    Console.WriteLine($"P1 = P2: {p1 == p2}");
 
     SPoint p3 = new SPoint { X = 7, Y = 3 };
 
-    //Console.WriteLine($"P1 = P3: {p1 == p3}");
+    Console.WriteLine($"P1 = P3: {p1 == p3}");
     Console.WriteLine();
 }
 
@@ -39,7 +39,7 @@ static void Records()
     RPoint p1 = new RPoint(7, 3);
     RPoint p2 = p1;
 
-    Console.WriteLine($"P1 = P2: {p1==p2}");
+    Console.WriteLine($"P1 = P2: {p1 == p2}");
 
     RPoint p3 = new RPoint(7, 3);
 
