ec268624fabb1b200281151d0b9fc413503814a4
brianmex7 <brianmexico77@gmail.com>
2024-09-04 18:45:26 -0600

    testing enum values

--- Ex_Files_C_Sharp_EssT_1/Exercise Files/CH05/03/Begin/LinkedIn.Essentials/LinkedIn.Essentials/Program.cs
+++ Ex_Files_C_Sharp_EssT_1/Exercise Files/CH05/03/Begin/LinkedIn.Essentials/LinkedIn.Essentials/Program.cs
@@ -1,12 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LinkedIn.Essentials;
 
-ShiftWorker d = new ShiftWorker() { FirstName = "Dynamic", 
-    LastName = "Worker" };
-d.DaysAvailable = ShiftDays.Weekdays | ShiftDays.Weekend;
+ShiftWorker d = new ShiftWorker()
+{
+    FirstName = "Dynamic",
+    LastName = "Worker"
+};
+d.DaysAvailable = ShiftDays.Weekdays;// | ShiftDays.Weekend;
+bool availableMonday = d.DaysAvailable.HasFlag(ShiftDays.Monday);
+Console.WriteLine($"Available Monday {availableMonday}");
+
+bool availableSaturdays = (d.DaysAvailable & ShiftDays.Saturday) == ShiftDays.Saturday;
+Console.WriteLine($"Available Saturday {availableSaturdays}");
 
 
 Console.WriteLine(d.DaysAvailable);
+
+var shiftName = System.Enum.GetNames(typeof(ShiftDays));
+Console.WriteLine(string.Join(',', shiftName));
+
+var shiftValues = (short[])System.Enum.GetValues(typeof(ShiftDays));
+Console.WriteLine(string.Join(". ", shiftValues));
 
 var day = DayOfWeek.Monday;
 //Console.WriteLine(day);
