2858aec5c052d2454c7dc6da29dc2845095153e7
brianmex7 <brianmexico77@gmail.com>
2024-09-05 18:49:59 -0600

    generics

--- DotNet7/Finished/Overview/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ DotNet7/Finished/Overview/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21c9895e53661add19c592e9ad84151a87f950c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ad84e4318a81888ee942cd4ea5699e1cb23a5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Ex_Files_C_Sharp_EssT_2/Ex_Files_C_Sharp_EssT_2/Exercise Files/Ch01/01/Begin/LinkedIn.Essentials2/LinkedIn.Essentials2/Program.cs
+++ Ex_Files_C_Sharp_EssT_2/Ex_Files_C_Sharp_EssT_2/Exercise Files/Ch01/01/Begin/LinkedIn.Essentials2/LinkedIn.Essentials2/Program.cs
@@ -1,10 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Essentials2.Library;
 
-
-static void Swap(object first, object second)
+int x = 5, y = 7;
+Swap<int>(ref x, ref y);
+Console.WriteLine($"X: {x} and Y: {y}");
+static void Swap<T>(ref T first, ref T second)
 {
-    object temp = second;
+    T temp = second;
     second = first;
     first = temp;
 }
+
+var p1 = new Person
+{
+    FirstName = "Brian",
+    LastName = "Bustos",
+    Age = 27
+};
+
+var p2 = new Person
+{
+    FirstName = "Carter",
+    LastName = "Marcell",
+    Age = 80
+};
+
+Swap<Person>(ref p1, ref p2);
+
+Console.WriteLine($"person 1 is {p1.FirstName}");
